277282fccdf560a4ab38a7b921a7b7758e1360eb
Sameera Chathuranga <studies.sameera@gmail.com>
2017-11-05 12:56:55 +0530

    Feat: Add a commin header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './common/header';
+
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+
+        <Header />
+
+        
+      </div>
     );
   }
 }
---
+++ src/components/common/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Header = () => {
+    return <header>
+        <h1>Readable</h1>
+    </header>
+}
