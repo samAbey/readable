c1da47c715afa3cfcf8fc73a4deae09725a517d5
Sameera Chathuranga <studies.sameera@gmail.com>
2017-11-05 13:13:34 +0530

    Style: Move header component to header folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './common/header';
+import Header from './common/header/header';
 
 class App extends Component {
   render() {
