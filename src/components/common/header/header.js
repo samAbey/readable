9945bf188d97112b7b73c2d704dc514fb6440c47
Sameera Chathuranga <studies.sameera@gmail.com>
2017-11-05 22:19:47 +0530

    Feat: Add link to default view

--- src/components/common/header/header.js
+++ src/components/common/header/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import * as styles from './header.css';
 
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return <header {...styles.headerStyles}>
-        <h1>Readable</h1>
+        <h1><Link to="/">Readable</Link></h1>
     </header>
 }
 
