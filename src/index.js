781e0381d4c6090122a7909d1ec6e741f699cd24
Sameera Chathuranga <studies.sameera@gmail.com>
2017-11-05 17:42:24 +0530

    Feat: Configuring redux

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(()=>({}),composeEnhancers(
+    applyMiddleware(thunk, logger)
+));
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
